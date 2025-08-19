7ef7dffa49eef0e8f144fe29573552369d9e6079
Cadeez <you@example.com>
2025-08-19 21:42:32 +0800

    changed file name to choose_booking

--- admin_side/amenity_booking.php
+++ admin_side/amenity_booking.php
@@ -233,7 +233,7 @@ $bookings_result = $conn->query($booking_sql);
                     <div class="tab-pane fade show active" id="bookings" role="tabpanel">
                         <div class="d-flex justify-content-between align-items-center mb-3">
                             <span class="small">List of Amenity Bookings</span>
-                            <a href="amenity_booking/add_booking.php" class="btn btn-primary btn-sm">+ Create New Booking</a>
+                            <a href="amenity_booking/choose_booking.php" class="btn btn-primary btn-sm">+ Create New Booking</a>
                         </div>
                         <div class="table-responsive">
                             <table class="table table-bordered table-hover">
