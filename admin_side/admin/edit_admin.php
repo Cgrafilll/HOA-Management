4f7c3d495bf9dcd16006afebc18df20e4346e275
Cgrafilll <cjgrafil1@gmail.com>
2025-08-21 15:39:18 +0800

    olrayt

--- admin_side/admin/edit_admin.php
+++ admin_side/admin/edit_admin.php
@@ -184,7 +184,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>NSSHAI HOA Management</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
     <link rel="icon" href="../images/SitioSeville_Logo.png" type="image/x-icon">
     <style>
--- admin_side/visitor/add_visitor.php
+++ admin_side/visitor/add_visitor.php
@@ -123,7 +123,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>NSSHAI HOA Management</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
     <link rel="icon" href="../images/SitioSeville_Logo.png" type="image/x-icon">
     <style>
@@ -424,7 +423,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                         <!-- Resident RFID -->
                         <div class="row">
                             <div class="col-md-4 mb-3">
-                                <label class="form-label mt-2 fw-bold">Resident RFID</label>
+                                <label class="form-label mt-2 fw-bold">Visitor RFID</label>
                                 <input type="text" name="rfid" class="form-control" id="rfidInput" required />
                                 <label class="form-label mt-2">Tap your RFID card</label>
                             </div>
