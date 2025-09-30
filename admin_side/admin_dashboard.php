4f6d229d2278359eb205a0b37df9d8d8e4914911
Cgrafilll <cjgrafil1@gmail.com>
2025-09-30 13:48:09 +0800

    dashboard

--- admin_side/admin_dashboard.php
+++ admin_side/admin_dashboard.php
@@ -354,7 +354,7 @@ $total_outstanding = $dues_total;
                 <div class="col-md-4">
                     <div class="card shadow-sm">
                         <div class="card-body d-flex align-items-center gap-3">
-                            <div class="bg-danger bg-opacity-10 rounded-5 p-3">
+                            <div class="bg-danger bg-opacity-10 rounded p-3">
                                 <span class="text-danger fs-4"><i class="bi bi-exclamation-octagon"></i></span>
                             </div>
                             <div>
@@ -367,7 +367,7 @@ $total_outstanding = $dues_total;
                 <div class="col-md-4">
                     <div class="card shadow-sm">
                         <div class="card-body d-flex align-items-center gap-3">
-                            <div class="bg-primary bg-opacity-10 rounded-5 p-3">
+                            <div class="bg-primary bg-opacity-10 rounded p-3">
                                 <span class="text-primary fs-4"><i class="bi bi-hourglass"></i></span>
                             </div>
                             <div>
@@ -380,7 +380,7 @@ $total_outstanding = $dues_total;
                 <div class="col-md-4">
                     <div class="card shadow-sm">
                         <div class="card-body d-flex align-items-center gap-3">
-                            <div class="bg-success bg-opacity-10 rounded-5">
+                            <div class="bg-success bg-opacity-10 rounded-5 p-3">
                                 <span class="text-success fs-4"><i class="bi bi-house-check"></i></span>
                             </div>
                             <div>
