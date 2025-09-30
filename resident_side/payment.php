ee2a3bf0dc753c4f719e5d07094c69f45a78b8f5
Cadeez <you@example.com>
2025-10-01 04:52:51 +0800

    1.3

--- resident_side/payment.php
+++ resident_side/payment.php
@@ -1051,7 +1051,7 @@ if (isset($_GET['action'])) {
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-                        <!--updated js-->
+                        <!--updated js ata-->
     <script src="payment.js"></script>
 </body>
 
