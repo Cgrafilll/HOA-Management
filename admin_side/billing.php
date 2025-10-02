6c15827e633fb6cfd5e3fde5c14cb8ceb1fda518
Cadeez <you@example.com>
2025-10-02 15:09:33 +0800

    fixing invoice to  billing

--- admin_side/billing.php
+++ admin_side/billing.php
@@ -531,8 +531,8 @@ function getNumericAmount($amountStr)
                     <!-- Button row -->
                     <div class="d-flex justify-content-between align-items-center mb-3">
                         <div class="fw-semibold">List of Billing Statements</div>
-                        <a href="invoice/add_invoice.php" class="btn btn-primary btn-sm d-flex align-items-center">
-                            <i class="bi bi-plus-lg me-1"></i> New Monthly Dues Billing
+                        <a href="invoice/add_billing.php" class="btn btn-primary btn-sm d-flex align-items-center">
+                            <i class="bi bi-plus-lg me-1"></i> New Billing
                         </a>
                     </div>
                     <!-- Filter Dropdown -->
