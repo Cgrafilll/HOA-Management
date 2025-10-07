7999e020ae1beb1623de337f96279d3867cd7d16
Cgrafilll <cjgrafil1@gmail.com>
2025-10-07 09:04:40 +0800

    admin responsive

--- admin_side/admin/view_admin.php
+++ admin_side/admin/view_admin.php
@@ -284,7 +284,8 @@ if ($view_admin_id) {
                 font-size: 1rem !important;
             }
 
-            .table-responsive {
+            .label,
+            .value {
                 font-size: 0.85rem;
             }
 
@@ -321,7 +322,8 @@ if ($view_admin_id) {
                 top: 0;
             }
 
-            .table-responsive {
+            .label,
+            .value {
                 font-size: 0.75rem;
             }
 
@@ -494,7 +496,7 @@ if ($view_admin_id) {
                                     </div>
                                 <?php endif; ?>
                             </div>
-                            <div class="mt-2 fw-semibold"><?php echo htmlspecialchars($roles); ?></div>
+                            <div class="mt-2 fw-semibold label"><?php echo htmlspecialchars($roles); ?></div>
                         </div>
                         <!-- Centered Grid for Labels + Values -->
                         <div class="d-flex justify-content-center">
@@ -516,8 +518,8 @@ if ($view_admin_id) {
                                 ];
                                 foreach ($details as $label => $value): ?>
                                     <div class="row mb-2">
-                                        <div class="col-4 text-start fw-bold"><?php echo $label ?>:</div>
-                                        <div class="col-8 text-start"><?php echo $value ?></div>
+                                        <div class="col-4 text-start fw-bold label"><?php echo $label ?>:</div>
+                                        <div class="col-8 text-start value"><?php echo $value ?></div>
                                     </div>
                                 <?php endforeach; ?>
                             </div>
