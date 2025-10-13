b1a135f4b708083c2eb7cca2d6db68745c284f93
Cadeez <you@example.com>
2025-10-13 23:18:15 +0800

    fixing resident invoices side

--- resident_side/invoices.php
+++ resident_side/invoices.php
@@ -344,7 +344,7 @@ function getCategoryIcon($category) {
                     </button>
                     <div class="collapse show" id="acctCollapse">
                         <ul class="nav flex-column ms-3 mt-1">
-                            <li><a href="payment.php" class="nav-link px-2">Payment</a></li>
+                            <li><a href="payment.php" class="nav-link px-2 actived">Payment</a></li>
                             <li><a href="billing.php" class="nav-link px-2">List of Billings</a></li>
                             <li><a href="invoices.php" class="nav-link px-2">Invoices</a></li>
                         </ul>
