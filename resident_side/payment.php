ea80c9657faad5b3958d2dff34eaae2694d203a9
Cadeez <you@example.com>
2025-10-13 23:19:02 +0800

    fixing resident invoices side

--- resident_side/payment.php
+++ resident_side/payment.php
@@ -929,7 +929,7 @@ if (isset($_GET['action'])) {
                     </button>
                     <div class="collapse show" id="acctCollapse">
                         <ul class="nav flex-column ms-3 mt-1">
-                            <li><a href="payment.php" class="nav-link px-2">Payment</a></li>
+                            <li><a href="payment.php" class="nav-link px-2 actived">Payment</a></li>
                             <li><a href="billing.php" class="nav-link px-2">List of Billings</a></li>
                             <li><a href="invoices.php" class="nav-link px-2">Invoices</a></li>
                         </ul>
